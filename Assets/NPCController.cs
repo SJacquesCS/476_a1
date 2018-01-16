7f165bb4d4b404f97a3c559ae8073dddc1db73c9
Jacques <s-jacques@live.com>
2018-01-16 14:16:28 -0500

    Modified movement

--- Assets/NPCController.cs
+++ Assets/NPCController.cs
@@ -5,26 +5,27 @@ using UnityEngine;
 public class NPCController : MonoBehaviour {
 
     public bool mType;
-    public int mSpeed;
+    public GameObject mTarget;
 
-    private Rigidbody mRb;
-    private Transform mTarget;
     private Vector3 mVelocity;
     private Vector3 mAngularVelocity;
 
     private void Awake()
     {
-        GameObject[] NPCs = GameObject.FindGameObjectsWithTag("NPC");
-        int randomizer = Random.Range(0, NPCs.Length);
-        mTarget = NPCs[randomizer].transform;
-        GetComponent<Rigidbody>().mass = Random.Range(1, 5);
-
-        mRb = GetComponent<Rigidbody>();
+        if (!mTarget)
+        {
+            GameObject[] NPCs = GameObject.FindGameObjectsWithTag("NPC");
+            int randomizer = Random.Range(0, NPCs.Length);
+            mTarget = NPCs[randomizer];
+        }
     }
 
     void Update () {
-        mVelocity = mSpeed * (1 / Mathf.Sqrt(2) * (mTarget.transform.position - transform.position).normalized);
 
-        mRb.velocity = mVelocity;
+        mVelocity = (mTarget.transform.position - transform.position) / Mathf.Sqrt(2);
+
+        Vector3 newPos = transform.position + (mVelocity * Time.deltaTime);
+
+        transform.position = newPos;
 	}
 }
