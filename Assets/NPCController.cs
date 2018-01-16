2be27fe8395ebe33991c6f278506aa26c85d451b
Jacques <s-jacques@live.com>
2018-01-15 23:48:28 -0500

    Added interactions

--- Assets/NPCController.cs
+++ Assets/NPCController.cs
@@ -4,13 +4,27 @@ using UnityEngine;
 
 public class NPCController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+    public bool mType;
+    public int mSpeed;
+
+    private Rigidbody mRb;
+    private Transform mTarget;
+    private Vector3 mVelocity;
+    private Vector3 mAngularVelocity;
+
+    private void Awake()
+    {
+        GameObject[] NPCs = GameObject.FindGameObjectsWithTag("NPC");
+        int randomizer = Random.Range(0, NPCs.Length);
+        mTarget = NPCs[randomizer].transform;
+        GetComponent<Rigidbody>().mass = Random.Range(1, 5);
+
+        mRb = GetComponent<Rigidbody>();
+    }
+
+    void Update () {
+        mVelocity = mSpeed * (1 / Mathf.Sqrt(2) * (mTarget.transform.position - transform.position).normalized);
+
+        mRb.velocity = mVelocity;
 	}
 }
